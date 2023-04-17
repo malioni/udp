181e4ab4455133b834a4dadfca71ed9c00938774
malioni <47998755+malioni@users.noreply.github.com>
2023-04-17 14:00:35 -0700

    Create server.cpp

---
+++ server.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+
+#define MAX_EVENTS 10
+#define BUF_SIZE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock_fd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event event, events[MAX_EVENTS];
+    int epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = sock_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &event) == -1) {
+        perror("epoll_ctl");
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[BUF_SIZE];
+    while (1) {
+        int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (n_ready == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < n_ready; i++) {
+            if (events[i].events & EPOLLIN) {
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_len = sizeof(client_addr);
+
+                int n_bytes = recvfrom(sock_fd, buf, sizeof(buf), 0,
+                                       (struct sockaddr *)&client_addr, &client_addr_len);
+                if (n_bytes == -1) {
+                    perror("recvfrom");
+                    exit(EXIT_FAILURE);
+                }
+
+                printf("Received message from %s:%d: %.*s\n",
+                       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),
+                       n_bytes, buf);
+            }
+        }
+    }
+
+    close(sock_fd);
+    return 0;
+}
