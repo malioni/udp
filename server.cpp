e35a05acffde83447520d02a45d4f466462d3890
malioni <47998755+malioni@users.noreply.github.com>
2023-04-18 12:38:43 -0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,59 +1,47 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
-#include <sys/epoll.h>
-#include <vector>
-#include <string>
 #include <map>
-#include <sstream>
-
-#define MAX_EVENTS 10
-#define BUF_SIZE 1024
-
-struct file_to_write
-{
-    std::string name;
-    std::string contents;
-};
+#include <algorithm>
+#include "udp.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <ports>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     
     std::map<std::string, file_to_write> m_sock_to_file;
     std::stringstream ss;
-
-    int port = atoi(argv[1]);
-
-    // Create a socket
-    int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock_fd < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
+    vector<int> ports;
+    vector<int> sockets;
+    
+    for (int i = 1; i < argc; i++)
+    {
+        ports.push_back(atoi(argv[i]));
     }
-
-    // Reset memory and specify IPv4, any IP address and port number
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(port);
-
-    // Bind socket's name to an address
-    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    
+    int ports_size = ports.size();
+    
+    // Create sockets
+    for (int i = 0; i < ports_size; i++)
+    {
+        sockets.append(create_socket());
+    }
+    
+    for (int i = 0; i < ports_size; i++)
+    {
+        // Configure IPv4 and port
+        struct sockaddr_in server_addr = configure_ip_and_port(ports.at(i));
+        
+        // Bind socket's name to address
+        if (bind(sockets.at(i), (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        {
         perror("bind");
         exit(EXIT_FAILURE);
+        }
+        
+        // Set socket to non-blocking
+        fcntl(sockets.at(i), F_SETFL, O_NONBLOCK);
+        
     }
-
-    // set socket to non-blocking mode
-    fcntl(sock_fd, F_SETFL, O_NONBLOCK);
     
     // Create epoll event and field descriptor
     struct epoll_event event, events[MAX_EVENTS];
@@ -65,14 +53,19 @@ int main(int argc, char *argv[]) {
 
     // Specify that events are incoming to the server
     event.events = EPOLLIN;
-    event.data.fd = sock_fd;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &event) == -1) {
-        perror("epoll_ctl");
-        exit(EXIT_FAILURE);
+    for (int i = 0; i < ports_size; i++)
+    {
+        event.data.fd = sockets.at(i);
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockets.at(i), &event) == -1)
+        {
+            perror("epoll_ctl");
+            exit(EXIT_FAILURE);
+        }
     }
 
     char buffer[BUF_SIZE];
     while (1) {
+        // Wait for events to happen
         int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
         if (n_ready < 0) {
             perror("epoll_wait");
@@ -80,42 +73,80 @@ int main(int argc, char *argv[]) {
         }
 
         for (int i = 0; i < n_ready; i++) {
-            if (events[i].data.fd == sock_fd) {
+            // Check that events occurred on on of the sockets
+            if ( std::find(sockets.begin(), sockets.end(), events[i].data.fd) != sockets.end() )
+            {
+                int sock_fd = events[i].data.fd;
                 struct sockaddr_in client_addr;
                 socklen_t client_addr_len = sizeof(client_addr);
                 
-                int n_bytes = recvfrom(sock_fd, buffer, sizeof(buffer), 0,
-                                       (struct sockaddr *)&client_addr, &client_addr_len);
+                // Receive the information
+                int n_bytes = recvfrom(sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_addr_len);
                 if (n_bytes < 0) {
                     perror("recvfrom");
                     exit(EXIT_FAILURE);
                 }
                 
+                // Convert IP address and port to string to be used as an identifier of the client
                 ss.str(std::string());
                 ss << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port);
                 std::cout << ss.str() << std::endl;
                 
+                // if a new client, the first message is the file name
                 if (m_sock_to_file.find(ss.str()) == m_sock_to_file.end())
                 {
-                    std::cout << "Assigning name" << std::endl;
                     m_sock_to_file[ss.str()].name = buffer;
-                    std::cout << m_sock_to_file[ss.str()].name << std::endl;
                 }
                 else
                 {
-                    std::cout << "Assigning contents" << std::endl;
-                    m_sock_to_file[ss.str()].contents += buffer;
-                    std::cout << m_sock_to_file[ss.str()].contents << std::endl;
+                    // if an existing client, check whether the message is the terminating message
+                    std::string msg(buffer);
+                    if (msg == TERMINATING_MSG)
+                    {
+                        // Send a response
+                        event.events = EPOLLIN | EPOLLOUT;
+                        event.data.fd = sock_fd;
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sock_fd, &event) == -1)
+                        {
+                            perror("epoll_ctl mod");
+                            exit(EXIT_FAILURE);
+                        }
+                        char* response = TERMINATING_MSG;
+                        int n_bytes = sendto(sock_fd, response, strlen(response), 0,(struct sockaddr *)&client_addr, &client_addr_len);
+                        if (n_bytes < 0) {
+                            perror("response failed");
+                            exit(EXIT_FAILURE);
+                        }
+                        // Revert back to inputs only
+                        event.events = EPOLLIN;
+                        event.data.fd = sock_fd;
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sock_fd, &event) == -1)
+                        {
+                            perror("epoll_ctl mod");
+                            exit(EXIT_FAILURE);
+                        }
+                        
+                        // Write file
+                        int write = write_file(m_sock_to_file[ss.str()]);
+                        // Delete client from map
+                        m_sock_to_file.erase(ss.str());
+                    }
+                    else
+                    {
+                        // Add the message to the contents of the file
+                        m_sock_to_file[ss.str()].contents << buffer;
+                    }
                 }
-
-                printf("Received message from %s:%d: %.*s\n",
-                       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),
-                       n_bytes, buffer);
             }
         }
     }
 
-    close(sock_fd);
+    // Close all ports
+    for (int i = 0; i < ports_size; i++)
+    {
+        close(sockets.at(i));
+    }
+    
     close(epoll_fd);
     return 0;
 }
