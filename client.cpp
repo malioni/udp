82b570f16b290143eba681fdca1319874db52162
malioni <47998755+malioni@users.noreply.github.com>
2023-04-17 19:55:21 -0700

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -101,12 +101,8 @@ int main(int argc, char *argv[]) {
             if (events[i].data.fd == sock_fd) {
                 // send as much of the file as possible
                 int size = std::max(n_total - n_sent, BUF_SIZE) - 1;
-                const char *buffer = file_contents.substr(n_sent, size).c_str();
-                std::cout << "BUFFER: ";
-                for (int i = 0; i < size; i++) {
-                    std::cout << *buffer[i];
-                }
-                std::cout << std::endl;
+                const char* buffer = file_contents.substr(n_sent, size).c_str();
+                std::cout << "BUFFER: " << buffer << std::endl;
                 int n_bytes = sendto(sock_fd, buffer, size, 0,(struct sockaddr *)&server_addr, sizeof(server_addr));
                 std::cout << "Sending data" << std::endl;
                 if (n_bytes < 0) {
