9d6f9c3d4c01ccbd1e1dedc8984cf171db2e76e3
malioni <47998755+malioni@users.noreply.github.com>
2023-04-17 18:17:52 -0700

    Update and rename server.cpp to server.c

--- server.c
+++ server.c
@@ -19,23 +19,27 @@ int main(int argc, char *argv[]) {
 
     int port = atoi(argv[1]);
 
+    // Create a socket
     int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock_fd == -1) {
+    if (sock_fd < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
+    // Reset memory and specify IPv4, any IP address and port number
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
 
-    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    // Bind socket's name to an address
+    if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
+    // Create epoll event and field descriptor
     struct epoll_event event, events[MAX_EVENTS];
     int epoll_fd = epoll_create1(0);
     if (epoll_fd == -1) {
@@ -43,6 +47,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Specify that events are incoming to the server
     event.events = EPOLLIN;
     event.data.fd = sock_fd;
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &event) == -1) {
@@ -53,19 +58,19 @@ int main(int argc, char *argv[]) {
     char buf[BUF_SIZE];
     while (1) {
         int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        if (n_ready == -1) {
+        if (n_ready < 0) {
             perror("epoll_wait");
             exit(EXIT_FAILURE);
         }
 
         for (int i = 0; i < n_ready; i++) {
-            if (events[i].events & EPOLLIN) {
+            if (events[i].data.fd == sock_fd) {
                 struct sockaddr_in client_addr;
                 socklen_t client_addr_len = sizeof(client_addr);
 
                 int n_bytes = recvfrom(sock_fd, buf, sizeof(buf), 0,
                                        (struct sockaddr *)&client_addr, &client_addr_len);
-                if (n_bytes == -1) {
+                if (n_bytes < 0) {
                     perror("recvfrom");
                     exit(EXIT_FAILURE);
                 }
@@ -78,5 +83,6 @@ int main(int argc, char *argv[]) {
     }
 
     close(sock_fd);
+    close(epoll_fd);
     return 0;
 }
