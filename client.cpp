858d1736256ee4430d84d962eab6345d54c92c20
malioni <47998755+malioni@users.noreply.github.com>
2023-04-18 12:38:39 -0700

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,40 +1,5 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
-#include <sys/epoll.h>
-#include <vector>
-#include <string>
-#include <fstream>
-
-#define MAX_EVENTS 10
-#define BUF_SIZE 1024
-
-int read_file(const std::string &file_path, std::string &file_contents)
-{
-    std::ifstream ifss(file_path); // open file
-    
-    if (ifss.is_open()) // check that file is open
-    {
-        ifss.seekg(0, std::ios::end); // set the get position to the end of the file
-        file_contents.reserve(ifss.tellg()); // reserve space in the string for the entire file
-        ifss.seekg(0, std::ios::beg); // set the get position back to the beginning of the file
-        file_contents.assign((std::istreambuf_iterator<char>(ifss)),
-                              std::istreambuf_iterator<char>()); // read file into string
-        ifss.close(); // close file
-    }
-    else 
-    {
-        return -1;
-    }
-    
-    return 0;
-}
-        
+#include "udp.hpp"
+   
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <port> <file>\n", argv[0]);
@@ -46,24 +11,17 @@ int main(int argc, char *argv[]) {
     
     std::string file_contents;
     // read the file
-    if (read_file(file_name, file_contents) < 0)
+    if (read_file(file_name, file_contents) == 0)
     {
         perror("Error reading the file");
         exit(EXIT_FAILURE);
     }
 
     // create a UDP socket
-    int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock_fd < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+    int sock_fd = create_socket();
 
     // Configure IPv4 and correct port
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+    struct sockaddr_in server_addr = configure_ip_and_port(port);
 
     // Create epoll event and it's file descriptor
     struct epoll_event event, events[MAX_EVENTS];
@@ -83,14 +41,35 @@ int main(int argc, char *argv[]) {
     
     // TODO: Start the clock
     
-    // TODO: Send the file name
 
+    // Send the file name
+    int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (n_ready < 0) {
+        perror("epoll_wait");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < n_ready; i++) {
+        if (events[i].data.fd == sock_fd) {
+            // send the file name
+            int size = file_name.length();
+            char buffer[size];
+            strcpy(buffer, file_name.c_str());
+            int n_bytes = sendto(sock_fd, buffer, size, 0,(struct sockaddr *)&server_addr, sizeof(server_addr));
+            if (n_bytes < 0) {
+                perror("send failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    // Send the file contents
     int n_sent = 0;
     int n_total = file_contents.length();
 
     while (n_sent < n_total) {
         // wait for socket to be available
-        int n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
         if (n_ready < 0) {
             perror("epoll_wait");
             exit(EXIT_FAILURE);
@@ -111,7 +90,55 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    // TODO: Send a finished message
+    // Send the terminating message
+    std::string msg(TERMINATING_MSG);
+    n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (n_ready < 0) {
+        perror("epoll_wait");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < n_ready; i++) {
+        if (events[i].data.fd == sock_fd) {
+            // send the file name
+            int size = msg.length();
+            char buffer[size];
+            strcpy(buffer, msg.c_str());
+            int n_bytes = sendto(sock_fd, buffer, size, 0,(struct sockaddr *)&server_addr, sizeof(server_addr));
+            if (n_bytes < 0) {
+                perror("send failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    
+    // Wait for response
+    event.events = EPOLLIN;
+    event.data.fd = sock_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sock_fd, &event) < 0) {
+        perror("epoll_ctl");
+        exit(EXIT_FAILURE);
+    }
+    
+    n_ready = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (n_ready < 0) {
+        perror("epoll_wait");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < n_ready; i++) {
+        if (events[i].data.fd == sock_fd) {
+            char buffer[BUF_SIZE];
+            
+            int n_bytes = recvfrom(sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&server_addr, &server_addr_len);
+            if (n_bytes < 0) {
+                perror("recvfrom");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    
+    
     
 //     // TODO: End the clock
 //     clock_t end_time = clock();
